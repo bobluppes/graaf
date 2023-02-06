2f9f9fd90da2566b615c5d5111a13a5661ed3ff6
Bob Luppes <bobluppes@gmail.com>
2023-02-06 18:43:15 +0100

    Fix examples

--- example/graaflib/example_2.cpp
+++ example/graaflib/example_2.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <iostream>
 
-#include <graaflib/graph.h>
+#include <graaflib/directed_graph.h>
 
 struct user_struct {
 	int number{};
@@ -12,23 +12,17 @@ struct user_struct {
 	}
 };
 
-struct user_struct_hash {
-	std::size_t operator()(const user_struct& key) const {
-		return std::hash<int>()(key.number) ^ (std::hash<std::string>()(key.data) << 1);
-	}
-};
-
 int main() {
 
 	const user_struct v1{3, "some string data"};
 	const user_struct v2{5, "some other string data"};
 
-	// const auto graph {graaf::graph<user_struct, user_struct_hash>::builder().vertices({
-    //     {0, v1},
-    //     {1, v2}
-    // }).edges({
-    //     {0, 1}
-    // }).build()};
+	const auto graph {graaf::directed_graph<user_struct>::builder().vertices({
+        {0, v1},
+        {1, v2}
+    }).edges({
+        {0, 1}
+    }).build()};
 
-    // std::cout << graph.vertex_count() << std::endl;
+    std::cout << graph.vertex_count() << std::endl;
 }
