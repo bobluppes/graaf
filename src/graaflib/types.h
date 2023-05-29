277f964c7553667500a620ebd70142f6ab6482fe
Bob Luppes <bobluppes@gmail.com>
2023-05-29 19:12:49 +0200

    make library header-only

--- src/graaflib/types.h
+++ src/graaflib/types.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstddef>
+#include <functional>
 #include <utility>
 
 namespace graaf {
@@ -11,7 +12,13 @@ using edge_id_t = std::size_t;
 using vertex_ids_t = std::pair<vertex_id_t, vertex_id_t>;
 
 struct vertex_ids_hash {
-  [[nodiscard]] std::size_t operator()(const vertex_ids_t& key) const;
+  [[nodiscard]] std::size_t operator()(const vertex_ids_t& key) const {
+    const auto h1{std::hash<vertex_id_t>{}(key.first)};
+    const auto h2{std::hash<vertex_id_t>{}(key.second)};
+
+    // TODO: use something like boost::hash_combine
+    return h1 ^ h2;
+  }
 };
 
 }  // namespace graaf
