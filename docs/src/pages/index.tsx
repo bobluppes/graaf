4669f62876e3d329cf7e0f8ead57250c1d5de463
Bob Luppes <bobluppes@gmail.com>
2023-06-03 23:25:48 +0200

    fix broken documentation build

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/tutorial/intro">
             Graaf Tutorial - 5min ⏱️
           </Link>
         </div>
