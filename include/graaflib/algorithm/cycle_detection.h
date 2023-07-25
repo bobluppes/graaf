a6e198be21ce51d113000ffbea9cbe475f1a7460
Aleksandr <hromizz@gmail.com>
2023-07-25 23:42:39 +0300

    DFS cycle detection (#42)
    Co-authored-by: Bob Luppes <bobluppes@gmail.com>

---
+++ include/graaflib/algorithm/cycle_detection.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <graaflib/graph.h>
+
+namespace graaf::algorithm {
+
+/*
+ * @brief Traverses the graph and checks for cycles.
+ *
+ * @param graph The graph to traverse.
+ */
+template <typename V, typename E, graph_spec S>
+[[nodiscard]] bool has_cycle(const graph<V, E, S> &graph);
+
+}  // namespace graaf::algorithm
+
+#include "cycle_detection.tpp"
