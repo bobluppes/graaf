5bd4449592f597189fe3a8074c9c92a673c31b04
Jonas Weich <jns.wch@gmail.com>
2023-07-30 21:52:28 +0200

    Refactor file into method

--- tools/benchmark_visualization.py
+++ tools/benchmark_visualization.py
@@ -1,7 +1,8 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from pathlib import Path
 import os
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import pandas as pd
 
 """
 This script can be used to visualize benchmark results. The script assumes multiple benchmark 
@@ -11,47 +12,50 @@ Graaf_perf executable:
 build/perf/Graaf_perf --benchmark_out=benchmark_results.csv --benchmark_out_format=csv
 """
 
-# Input variables
-benchmark_dir = Path("../build")  # The directory containing the csv files
-benchmark_filter = "bm_"  # Option to filter on specific benchmark names
 
-# Constants
-google_benchmark_skiprows = 10
+def plot_benchmarks_from_csv(
+    benchmark_dir=Path("../build"), benchmark_filter="bm_", google_benchmark_skiprows=10
+):
+    plt.figure()
 
-plt.figure()
-
-for benchmark_results in benchmark_dir.glob("*.csv"):
-    filename = os.path.basename(benchmark_results)
-    benchmarks_df = pd.read_csv(benchmark_results, skiprows=google_benchmark_skiprows)
-
-    # The benchmark name and variable are recorded as 'bm_name/variable'
-    benchmarks_df[["bm_name", "variable"]] = benchmarks_df["name"].str.split(
-        pat="/", n=1, expand=True
-    )
-    benchmarks_df["variable"] = benchmarks_df["variable"].astype(int)
-
-    # Google benchmark records the time in ns
-    benchmarks_df["time (s)"] = benchmarks_df["real_time"].apply(lambda x: x * 1e-9)
-
-    # Filter
-    benchmarks_df = benchmarks_df[
-        benchmarks_df["bm_name"].apply(lambda x: x.find(benchmark_filter) != -1)
-    ]
-
-    for benchmark_df in [x for _, x in benchmarks_df.groupby("bm_name")]:
-        benchmark_label = "{} - {}".format(
-            os.path.splitext(filename)[0], benchmark_df["bm_name"].iloc[0]
+    for benchmark_results in benchmark_dir.glob("*.csv"):
+        filename = os.path.basename(benchmark_results)
+        benchmarks_df = pd.read_csv(
+            benchmark_results, skiprows=google_benchmark_skiprows
         )
 
-        plt.plot(
-            benchmark_df["variable"],
-            benchmark_df["time (s)"],
-            label=benchmark_label,
-            marker=".",
+        # The benchmark name and variable are recorded as 'bm_name/variable'
+        benchmarks_df[["bm_name", "variable"]] = benchmarks_df["name"].str.split(
+            pat="/", n=1, expand=True
         )
+        benchmarks_df["variable"] = benchmarks_df["variable"].astype(int)
 
-plt.xlabel("number of additions")
-plt.ylabel("runtime (s)")
-plt.xscale("log")
-plt.legend()
-plt.show()
+        # Google benchmark records the time in ns
+        benchmarks_df["time (s)"] = benchmarks_df["real_time"].apply(lambda x: x * 1e-9)
+
+        # Filter
+        benchmarks_df = benchmarks_df[
+            benchmarks_df["bm_name"].apply(lambda x: x.find(benchmark_filter) != -1)
+        ]
+
+        for benchmark_df in [x for _, x in benchmarks_df.groupby("bm_name")]:
+            benchmark_label = "{} - {}".format(
+                os.path.splitext(filename)[0], benchmark_df["bm_name"].iloc[0]
+            )
+
+            plt.plot(
+                benchmark_df["variable"],
+                benchmark_df["time (s)"],
+                label=benchmark_label,
+                marker=".",
+            )
+
+    plt.xlabel("number of additions")
+    plt.ylabel("runtime (s)")
+    plt.xscale("log")
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    plot_benchmarks_from_csv()
