b5db653e17ac541c5a3dccc0073564c01bf60fc3
Hromz <hromizz@gmail.com>
2023-08-18 20:43:02 +0300

    clang style correction

--- test/graaflib/algorithm/minimum_spanning_tree_test.cpp
+++ test/graaflib/algorithm/minimum_spanning_tree_test.cpp
@@ -62,7 +62,7 @@ TYPED_TEST(MSTTest, SparseGraph) {
 
   auto mst = graaf::algorithm::kruskal_minimum_spanning_tree(graph);
   std::vector<edge_id_t> expected_mst{
-      {vertex_1, vertex_3}, {vertex_3, vertex_4}, {vertex_2, vertex_4}}; 
+      {vertex_1, vertex_3}, {vertex_3, vertex_4}, {vertex_2, vertex_4}};
 
   ASSERT_EQ(expected_mst, mst);
 }
@@ -93,13 +93,10 @@ TYPED_TEST(MSTTest, TwoComponents) {
   graph.add_edge(vertex_8, vertex_6, 0.15f);
 
   auto mst = graaf::algorithm::kruskal_minimum_spanning_tree(graph);
-  std::vector<edge_id_t> expected_mst {{vertex_8, vertex_9},
-                                      {vertex_3, vertex_4},
-                                      {vertex_2, vertex_4},
-                                      {vertex_6, vertex_8},
-                                      {vertex_1, vertex_3},
-                                      {vertex_5, vertex_6},
-                                      {vertex_7, vertex_8}};
+  std::vector<edge_id_t> expected_mst{
+      {vertex_8, vertex_9}, {vertex_3, vertex_4}, {vertex_2, vertex_4},
+      {vertex_6, vertex_8}, {vertex_1, vertex_3}, {vertex_5, vertex_6},
+      {vertex_7, vertex_8}};
 
   ASSERT_EQ(expected_mst, mst);
 }
@@ -136,7 +133,7 @@ TYPED_TEST(MSTTest, CompleteGraph) {
   graph.add_edge(vertex_1, vertex_4, 45);
   graph.add_edge(vertex_2, vertex_4, 6);
   graph.add_edge(vertex_2, vertex_3, 55);
-  graph.add_edge(vertex_3, vertex_4, 5);   
+  graph.add_edge(vertex_3, vertex_4, 5);
 
   auto mst = graaf::algorithm::kruskal_minimum_spanning_tree(graph);
   std::vector<edge_id_t> expected_mst{
@@ -170,8 +167,8 @@ struct custom_edge : public graaf::weighted_edge<int> {
   int weight_{};
 
   [[nodiscard]] int get_weight() const noexcept override { return weight_; }
- 
-  custom_edge(int weight): weight_{weight} {};
+
+  custom_edge(int weight) : weight_{weight} {};
   custom_edge(){};
   ~custom_edge(){};
 
@@ -185,24 +182,22 @@ struct custom_edge : public graaf::weighted_edge<int> {
 };
 
 TYPED_TEST(MSTTest, CustomEdgeVertexOneComponentGraph) {
-    undirected_graph<custom_vertex, custom_edge> graph{};
+  undirected_graph<custom_vertex, custom_edge> graph{};
 
-    const auto vertex_1 = graph.add_vertex(custom_vertex{1, "Custom vertex 1"});
-    const auto vertex_2 = graph.add_vertex(custom_vertex{2, "Custom vertex 2"});
-    const auto vertex_3 = graph.add_vertex(custom_vertex{3, "Custom vertex 3"});
-    const auto vertex_4 = graph.add_vertex(custom_vertex{4, "Custom vertex 4"});
+  const auto vertex_1 = graph.add_vertex(custom_vertex{1, "Custom vertex 1"});
+  const auto vertex_2 = graph.add_vertex(custom_vertex{2, "Custom vertex 2"});
+  const auto vertex_3 = graph.add_vertex(custom_vertex{3, "Custom vertex 3"});
+  const auto vertex_4 = graph.add_vertex(custom_vertex{4, "Custom vertex 4"});
 
-    graph.add_edge(vertex_3, vertex_1, 4);
-    graph.add_edge(vertex_2, vertex_4, 6);
-    graph.add_edge(vertex_3, vertex_4, 5);
-    graph.add_edge(vertex_1, vertex_2, 16);
+  graph.add_edge(vertex_3, vertex_1, 4);
+  graph.add_edge(vertex_2, vertex_4, 6);
+  graph.add_edge(vertex_3, vertex_4, 5);
+  graph.add_edge(vertex_1, vertex_2, 16);
 
-    auto mst = graaf::algorithm::kruskal_minimum_spanning_tree(graph);
-    std::vector<edge_id_t> expected_mst{
-        {vertex_1, vertex_3},
-        {vertex_3, vertex_4},
-        {vertex_2, vertex_4}}; 
+  auto mst = graaf::algorithm::kruskal_minimum_spanning_tree(graph);
+  std::vector<edge_id_t> expected_mst{
+      {vertex_1, vertex_3}, {vertex_3, vertex_4}, {vertex_2, vertex_4}};
 
-    ASSERT_EQ(expected_mst, mst);
+  ASSERT_EQ(expected_mst, mst);
 }
 }  // namespace graaf::algorithm
