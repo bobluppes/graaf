1f0cc863cfe7b6773bea11473a775ab507140bd3
Jonas Weich <jns.wch@gmail.com>
2023-10-01 11:38:04 +0200

    Fully connected graph scenario

--- test/utils/scenarios/scenarios.h
+++ test/utils/scenarios/scenarios.h
@@ -90,6 +90,53 @@ template <typename GRAPH_T>
 template <typename GRAPH_T>
 [[nodiscard]] scenario<GRAPH_T> create_simple_graph_scenario();
 
+/**
+ * Creates a scenario containing a fully connected graph structure. In the
+ * visualization below, vertices and edge values are shown. Vertex IDs are given
+ * between parentheses.
+ *
+ * The direction of the edges is visualized. However, undirected edges are
+ * supported as well if an undirected graph is passed as a template parameter.
+ *
+ *
+ *                   ┌─────────────────┐
+ *                   │                 │
+ *                 ┌────────┐          │
+ *           ┌──── │ 10 (0) │ ─┐       │
+ *           │     └────────┘  │       │
+ *           │       │         │       │
+ *           │       │ 100     │       │
+ *           │       ▼         │       │
+ *           │     ┌────────┐  │       │
+ *      ┌────┼──── │ 20 (1) │ ─┼───────┼────────────┐
+ *      │    │     └────────┘  │       │            │
+ *      │    │       │         │       │            │
+ *      │    │       │ 500     │ 200   │            │
+ *      │    │       ▼         │       │            │
+ *      │    │     ┌────────┐  │       │            │
+ * ┌────┼────┼──── │ 30 (2) │ ◀┘       │            │
+ * │    │    │     └────────┘          │            │
+ * │    │    │       │                 │            │
+ * │    │    │ 300   │ 800             │            │ 700
+ * │    │    │       ▼                 │            │
+ * │    │    │     ┌────────┐  600     │            │
+ * │    │    └───▶ │ 40 (3) │ ◀────────┼───────┐    │
+ * │    │          └────────┘          │       │    │
+ * │    │            │                 │       │    │
+ * │    │            │ 1000            │ 400   │    │
+ * │    │            ▼                 ▼       │    │
+ * │    │   900    ┌────────────────────────┐  │    │
+ * └────┼────────▶ │         50 (4)         │ ◀┼────┘
+ *      │          └────────────────────────┘  │
+ *      │                                      │
+ *      └──────────────────────────────────────┘
+ *
+ * @tparam GRAPH_T Can be either a directed or undirected graph with numeric
+ * vertices and edges.
+ * @return The scenario containing the graph structure.
+ */
+template <typename GRAPH_T>
+[[nodiscard]] scenario<GRAPH_T> create_fully_connected_graph_scenario();
 }  // namespace graaf::utils::scenarios
 
 #include "scenarios.tpp"
