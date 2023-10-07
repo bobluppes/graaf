d378c811fb47fdfd6ec96fdc2a890482589150d9
toadkarter <vrakhmanin1@gmail.com>
2023-10-07 23:24:52 +0100

    Updating javadoc on get_transposed_graph

--- include/graaflib/algorithm/utils.h
+++ include/graaflib/algorithm/utils.h
@@ -8,8 +8,7 @@ namespace graaf {
  * Get transposed version of a given directed graph
  *
  * @param  graph The directed graph that is to be transposed
- * @param  out_transposed_graph An out parameter that will contain the
- * transposed graph
+ * @return directed_graph<VERTEX_T, EDGE_T> The transposed graph
  */
 template <typename VERTEX_T, typename EDGE_T>
 directed_graph<VERTEX_T, EDGE_T> get_transposed_graph(
