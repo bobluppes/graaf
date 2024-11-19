f826ab66d8ea36e7c7702a4447db9a1622c54b89
Jonas Weich <jns.wch@gmail.com>
2024-11-19 21:58:04 +0100

    Use system-agnostic temp directory

--- test/graaflib/io/dot_test.cpp
+++ test/graaflib/io/dot_test.cpp
@@ -74,7 +74,8 @@ inline std::pair<vertex_id_t, vertex_id_t> make_sorted_pair(
 
 TEST(DotTest, EmptyUndirectedGraph) {
   // GIVEN
-  const std::filesystem::path path{"./DotTest_EmptyUndirectedGraph.dot"};
+  const std::filesystem::path path{std::filesystem::temp_directory_path() /
+                                   "DotTest_EmptyUndirectedGraph.dot"};
   undirected_graph<int, int> graph{};
 
   // WHEN
@@ -87,7 +88,8 @@ TEST(DotTest, EmptyUndirectedGraph) {
 
 TEST(DotTest, EmptyDirectedGraph) {
   // GIVEN
-  const std::filesystem::path path{"./DotTest_EmptyDirectedGraph.dot"};
+  const std::filesystem::path path{std::filesystem::temp_directory_path() /
+                                   "DotTest_EmptyDirectedGraph.dot"};
   directed_graph<int, int> graph{};
 
   // WHEN
@@ -100,7 +102,8 @@ TEST(DotTest, EmptyDirectedGraph) {
 
 TEST(DotTest, UndirectedGraph) {
   // GIVEN
-  const std::filesystem::path path{"./DotTest_UndirectedGraph.dot"};
+  const std::filesystem::path path{std::filesystem::temp_directory_path() /
+                                   "DotTest_UndirectedGraph.dot"};
   undirected_graph<int, int> graph{};
 
   const auto vertex_1{graph.add_vertex(10)};
@@ -144,7 +147,8 @@ TEST(DotTest, UndirectedGraph) {
 
 TEST(DotTest, DirectedGraph) {
   // GIVEN
-  const std::filesystem::path path{"./DotTest_DirectedGraph.dot"};
+  const std::filesystem::path path{std::filesystem::temp_directory_path() /
+                                   "DotTest_DirectedGraph.dot"};
   directed_graph<int, int> graph{};
 
   const auto vertex_1{graph.add_vertex(10)};
@@ -199,7 +203,8 @@ TEST(DotTest, UserProvidedVertexAndEdgeClass) {
   };
 
   const std::filesystem::path path{
-      "./DotTest_UserProvidedVertexAndEdgeClass.dot"};
+      std::filesystem::temp_directory_path() /
+      "DotTest_UserProvidedVertexAndEdgeClass.dot"};
   directed_graph<vertex_t, edge_t> graph{};
 
   const auto vertex_1{graph.add_vertex(vertex_t{10, "vertex 1"})};
@@ -229,7 +234,8 @@ TEST(DotTest, UserProvidedVertexAndEdgeClass) {
 
 TEST(DotTest, DefaultWriters) {
   // GIVEN
-  const std::filesystem::path path{"./DotTest_DefaultWriters.dot"};
+  const std::filesystem::path path{std::filesystem::temp_directory_path() /
+                                   "DotTest_DefaultWriters.dot"};
   directed_graph<int, float> graph{};
 
   const auto vertex_1{graph.add_vertex(10)};
