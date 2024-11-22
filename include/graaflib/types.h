463b7284aacf8bc67a51a470d7b3fbb7126ec52f
Laurence Emms <laurence@atomic.industries>
2024-11-22 12:25:36 -0800

    Added a hash combiner for performance improvement.

--- include/graaflib/types.h
+++ include/graaflib/types.h
@@ -9,13 +9,20 @@ namespace graaf {
 using vertex_id_t = std::size_t;
 using edge_id_t = std::pair<vertex_id_t, vertex_id_t>;
 
+template <class T>
+inline void hash_combine(std::size_t& seed, const T& v)
+{
+    std::hash<T> hasher;
+    seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
 struct edge_id_hash {
   [[nodiscard]] std::size_t operator()(const edge_id_t& key) const {
-    const auto h1{std::hash<vertex_id_t>{}(key.first)};
-    const auto h2{std::hash<vertex_id_t>{}(key.second)};
+    size_t seed = 0;
+    hash_combine(seed, key.first);
+    hash_combine(seed, key.second);
 
-    // TODO: use something like boost::hash_combine
-    return h1 ^ h2;
+    return seed;
   }
 };
 
