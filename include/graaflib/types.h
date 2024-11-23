78e1c8a728e0049b85906a01d81543875380a514
Laurence Emms <laurence@atomic.industries>
2024-11-22 16:05:12 -0800

    Fixed clang formatting

--- include/graaflib/types.h
+++ include/graaflib/types.h
@@ -10,10 +10,9 @@ using vertex_id_t = std::size_t;
 using edge_id_t = std::pair<vertex_id_t, vertex_id_t>;
 
 template <class T>
-inline void hash_combine(std::size_t& seed, const T& v)
-{
-    std::hash<T> hasher;
-    seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+inline void hash_combine(std::size_t& seed, const T& v) {
+  std::hash<T> hasher;
+  seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
 }
 
 struct edge_id_hash {
